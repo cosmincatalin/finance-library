2e2faa5fa3f7540ed20ce33d71b0dc4c9ed668fc
Cosmin Sanda <cosmincatalin@gmail.com>
2022-09-27 15:04:26 +0200

    sd

--- Tests/EarningsCalendarTest.cs
+++ Tests/EarningsCalendarTest.cs
@@ -7,8 +7,8 @@ namespace Tests;
 
 public class EarningsCalendarTest
 {
-    [Test, Order(1)]
-    public async Task FetchEarningsAndCache()
+    [Test]
+    public async Task FetchEarnings()
     {
         var ztoEarnings = await EarningsCalendar.GetPastEarningsDates("MSFT");
         Assert.Greater(
@@ -18,32 +18,10 @@ public class EarningsCalendarTest
         );
     }
 
-    [Test, Order(2)]
-    public async Task LoadCachedEarnings()
-    {
-        // var earnings = await Persister.GetCachedEarnings(Ticker);
-        // Assert.Greater(earnings.Count, 0, $"There must be at least several earning dates for {Ticker}.");
-    }
-
-    [Test, Order(3)]
-    public async Task LoadUnknownCachedEarnings()
-    {
-        // var earnings = await Persister.GetCachedEarnings("UNKNOWN");
-        // Assert.AreEqual(0, earnings.Count, "There must be 0 earnings fro UNKNOWN");
-    }
-
-    [Test, Order(4)]
+    [Test]
     public async Task GetNextEarningsDate()
     {
-        // var date = await EarningsCalendar.GetNextEarningsDate(Ticker);
-        // Assert.NotNull(date, "The date should not be null");
-    }
-
-    [Test, Order(0)]
-    public async Task FetchEarningsAndCacheByDay()
-    {
-        // DateTime day = new DateTime(2022, 7, 6);
-        // var earnings = await EarningsCalendar.GetCompaniesReporting(day);
-        // Assert.Greater(earnings.Count, 0, $"There must be at least several earning dates for {Ticker}.");
+        var date = await EarningsCalendar.GetNextEarningsDate("MSFT");
+        Assert.NotNull(date, "The date should not be null");
     }
 }
